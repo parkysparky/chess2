c726493c0f1903d92d3596097ee1f48041045c8d
parkysparky <sp474@byu.edu>
2025-02-24 22:33:48 -0700

    Install logging dependencies. main method now creates a server

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,22 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        if (0 < args.length) {
+            int serverPort = Integer.parseInt(args[0]);
+            Server server = new Server();
+            try {
+                serverPort = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid port number. Using default port: 8080");
+                serverPort = 8080;
+            }
+            server.run(serverPort);
+        }
+        else {
+            Server server = new Server();
+            server.run(8080);
+        }
     }
 }
