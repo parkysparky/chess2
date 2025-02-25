75ae77f33162f523bdce571e75fb22bc520dc74d
parkysparky <sp474@byu.edu>
2025-02-25 01:01:07 -0700

    refactored portNum

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -4,14 +4,14 @@ import server.Server;
 public class Main {
     public static void main(String[] args) {
         Server server = new Server();
-        int serverPort = 8080;
+        int portNumber = 8080;
         if (0 < args.length) {
             try {
-                serverPort = Integer.parseInt(args[0]);
+                portNumber = Integer.parseInt(args[0]);
             } catch (NumberFormatException e) {
                 System.out.println("Invalid port number. Using default port: 8080");
             }
         }
-        server.run(serverPort);
+        server.run(portNumber);
     }
 }
