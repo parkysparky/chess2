13a5bbde9c097c6bed1029db8fc40daf55985843
parkysparky <sp474@byu.edu>
2025-02-25 00:57:12 -0700

    improved the flow by removing the else statement in the server initialization and duplicate code

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -3,19 +3,15 @@ import server.Server;
 
 public class Main {
     public static void main(String[] args) {
+        Server server = new Server();
+        int serverPort = 8080;
         if (0 < args.length) {
-            Server server = new Server();
-            int serverPort = 8080;
             try {
                 serverPort = Integer.parseInt(args[0]);
             } catch (NumberFormatException e) {
                 System.out.println("Invalid port number. Using default port: 8080");
             }
-            server.run(serverPort);
         }
-        else {
-            Server server = new Server();
-            server.run(8080);
-        }
+        server.run(serverPort);
     }
 }
