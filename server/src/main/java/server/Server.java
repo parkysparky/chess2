93b11988ad649b3e10efe38e82d61accaa43d5bd
parkysparky <sp474@byu.edu>
2025-02-27 00:05:16 -0700

    created generic deserialize function for Routes to call

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,8 @@
 package server;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import server.service.request.RegisterRequest;
 import spark.*;
 
 public class Server {
@@ -10,6 +13,7 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        createRoutes();
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
@@ -22,4 +26,33 @@ public class Server {
         Spark.stop();
         Spark.awaitStop();
     }
+
+    private static void createRoutes(){
+        //TODO: write a before method for authentication
+        //register
+        Spark.post("/user", (req, res) -> {
+            //get and deserialize body
+            RegisterRequest registerRequest = deserialize(req.body(), RegisterRequest.class);
+            //send req data to service class
+
+            //receive service class response
+            //serialize service class stuff
+            //put service class stuff into response object
+
+            //return response object
+            return res;
+        });
+
+
+
+    }
+
+    private static <T extends Record> T deserialize(String json, Class<T> yourClass){
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        T object = gson.fromJson(json, yourClass);
+
+        return object;
+    }
+
 }
