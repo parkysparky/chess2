344f6bb5997fc8c80bbaa9556cbee30cddc043fa
parkysparky <sp474@byu.edu>
2025-03-11 21:38:37 -0600

    Autogenerated Tests. Need to write them and create the fail cases too

---
+++ server/src/test/java/service/GameServiceTest.java
@@ -0,0 +1,29 @@
+package service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listGames() {
+    }
+
+    @Test
+    void createGame() {
+    }
+
+    @Test
+    void joinGame() {
+    }
+
+    @Test
+    void resetGameData() {
+    }
+}
