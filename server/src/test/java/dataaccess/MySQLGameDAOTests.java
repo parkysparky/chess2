c8591c20d5b79c30808e86fbd9721cddcedd9eb9
parkysparky <sp474@byu.edu>
2025-03-19 17:02:47 -0600

    Set up the member objects, constructor, and createGame() tests

--- server/src/test/java/dataaccess/MySQLGameDAOTests.java
+++ server/src/test/java/dataaccess/MySQLGameDAOTests.java
@@ -1,18 +1,44 @@
 package dataaccess;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MySQLGameDAOTests {
+    MySQLAuthDAO mySQLAuthDAO;
+    MySQLUserDAO mySQLUserDAO;
+    MySQLGameDAO mySQLGameDAO;
+
+    int gameID;
+    final String gameName = "newGame";
+
+    public MySQLGameDAOTests() throws DataAccessException {
+        mySQLAuthDAO = new MySQLAuthDAO();
+        mySQLGameDAO = new MySQLGameDAO();
+        mySQLUserDAO = new MySQLUserDAO();
+    }
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws DataAccessException {
+        mySQLAuthDAO.clear();
+        mySQLGameDAO.clear();
+        mySQLUserDAO.clear();
+
+        gameID = mySQLGameDAO.createGame(gameName);
     }
 
     @Test
-    void createGame() {
+    void successCreateGame() throws DataAccessException { //depends on clear() and getGame()
+        Assertions.assertEquals(gameID, mySQLGameDAO.getGame(gameID).gameID(),
+                "generated and found gameIDs do not match");
+    }
+
+    @Test
+    void createGameNoName() throws DataAccessException { //depends on clear() and getGame()
+        Assertions.assertEquals(gameID, mySQLGameDAO.getGame(gameID).gameID(),
+                "generated and found gameIDs do not match");
     }
 
     @Test
