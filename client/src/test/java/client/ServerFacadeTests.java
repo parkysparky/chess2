db71393c1be2a5015e3fab1d9f118f02c8ad95d4
parkysparky <sp474@byu.edu>
2025-03-31 21:55:55 -0600

    Wrote test for clear

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,9 +2,14 @@ package client;
 
 import exception.ResponseException;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import server.Server;
 import server.ServerFacade;
 import server.result.RegisterResult;
+
+import java.util.stream.Stream;
 
 
 public class ServerFacadeTests {
@@ -12,9 +17,9 @@ public class ServerFacadeTests {
     private static Server server;
     private static ServerFacade serverFacade;
 
-    private final String testUser1 = "testUser1";
-    private final String password = "password";
-    private final String email = "example@email.com";
+    private static final String testUser1 = "testUser1";
+    private static final String password = "password";
+    private static final String email = "example@email.com";
 
     @BeforeAll
     public static void init() throws ResponseException {
@@ -32,11 +37,34 @@ public class ServerFacadeTests {
 
 
     @Test
+    public void normalClear() {
+        Assertions.assertDoesNotThrow(() -> serverFacade.clear(), "clear() throws an exception");
+    }
+
+    @Test
     public void normalRegisterUser() throws ResponseException {
         RegisterResult registerResult = serverFacade.register(testUser1, password, email);
 
         Assertions.assertNotNull(registerResult, "serverFacade.register returned null");
         Assertions.assertEquals(testUser1, registerResult.username());
     }
+
+    @ParameterizedTest()
+    @MethodSource("registerMissingDataTestCases")
+    public void registerUserDataMissing(String username, String password, String email) throws ResponseException {
+        Assertions.assertThrows(ResponseException.class, () -> serverFacade.register(username, email, password));
+    }
+
+    private static Stream registerMissingDataTestCases(){
+        return Stream.of(
+                Arguments.of(null, password, email),
+                Arguments.of(testUser1, null, email),
+                Arguments.of(testUser1, password, null),
+                Arguments.of(" ", password, email),
+                Arguments.of(testUser1, " ", email),
+                Arguments.of(testUser1, password, " ")
+        );
+    }
+
 
 }
